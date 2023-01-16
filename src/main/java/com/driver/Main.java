aef9aa4de657c0c6c8f463baad45f188a89c5547
sahilkumar013 <sahilkashyap7056@gmail.com>
2023-01-16 19:40:51 +0530

    Method Overriding

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,24 @@
 package com.driver;
 
+import java.sql.SQLOutput;
+
+import static javax.print.attribute.standard.MediaSizeName.B;
+
 public class Main {
+
+    public static class A{
+        public static String meth(){
+            return "Invoking method from class A";
+        }
+    }
+    public static class B extends A{
+        public static String meth(){
+            return "Method is overridden in Extendend class B";
+        }
+    }
+    public static  void main(String[]args){
+        B obj = new B();
+        System.out.println((obj.meth()));
+    }
   
 }
